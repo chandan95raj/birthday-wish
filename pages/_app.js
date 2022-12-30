74ed98706fe871253eed437854e746d2bf1de727
chandan raj <chandanraaz91@gmail.com>
2022-12-30 19:14:38 +0530

    animate.style library added master updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css';
 import '@fontsource/poppins/500.css';
 import 'animate.css/animate.min.css';
+import {
+  createTheme, ThemeProvider
+} from "@mui/material";
 
   function MyApp({ Component, pageProps }) {
   const Theme = createTheme({typography:{ fontFamily:"Poppins" }});
